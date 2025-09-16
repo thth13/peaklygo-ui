d1d7edd5ab1409bb9e6ffb843cdd48edcc062f2c
Dmitriy Khutornoy <thth13q@gmail.com>
2025-09-16 10:35:19 +0300

    upd google id

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
 export const SOCKET_URL = API_URL.replace(/\/api$/, '');
 export const IMAGE_URL = 'https://dh5p0367pyzhh.cloudfront.net';
-export const GOOGLE_CLIENT_ID = '853830546263-7jh0en2tn5i292pfg7l0a3v8hodjmr1s.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = '853830546263-to4b0e70kei8vghhprk9pudukoo8ntt0.apps.googleusercontent.com';
 
 export const GOALS_PER_PAGE = 10;
